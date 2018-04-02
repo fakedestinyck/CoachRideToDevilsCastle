e2eb8b26bbb28375150ae69fb7ceef9f40fc0548
Haochang Qian <fakedestinyck@gmail.com>
2018-04-02 18:20:47 -0400

    init repo

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码长度至少6位。',
+    'reset' => '密码成功重置！',
+    'sent' => '已经将密码重置链接通过邮件发送给您了。',
+    'token' => '密码重置校验失败！',
+    'user' => "未找到该邮箱地址所对应的用户名。",
+
+];
